ce94a5de3c82ad120aef9e84265152fc766e6706
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:29:04 +0300

    Create Class1.cs

---
+++ задача 23/Class1.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите начало интервала");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Введите конец интервала");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int sumi = 0;
+            if (b >= a)
+            {
+                for (int i = a; i <= b; i++)
+                {
+                    sumi += i;
+                }
+                Console.WriteLine("Сумма: " + sumi.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Ошибка: Начало диапозона больше конца");
+            }
+        }
+    }
+}
