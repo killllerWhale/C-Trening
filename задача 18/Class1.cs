fe0760f9e4986543c49485ae270bc845691fd94f
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:19 +0300

    Create Class1.cs

---
+++ задача 18/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        string rus = Convert.ToString(Console.ReadLine());
+        int counter = 0;
+
+        for (int i = 0; i < rus.Length; i++)
+        {
+            if (rus[i] == 'а')
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine(rus.Replace("а", "о"));
+        Console.WriteLine("Кол-во замен: " + counter.ToString());
+        Console.WriteLine("Символов в строке: " + (rus.Length).ToString());
+    }
+}
