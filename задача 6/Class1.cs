85cb33f7f285a28dc976602080addebc250f81ce
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:36 +0300

    Create Class1.cs

---
+++ задача 6/Class1.cs
@@ -0,0 +1,25 @@
+﻿uusing System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 0)
+            {
+                Console.WriteLine("Число положительное");
+            }
+            else if (n == 0)
+            {
+                Console.WriteLine("Число равно нулю");
+            }
+            else
+            {
+                Console.WriteLine("Число отрицательное");
+            }
+        }
+    }
+}
