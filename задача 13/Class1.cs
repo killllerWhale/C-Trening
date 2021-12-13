a5e1f36ae2a1a12ca244a0eb264487fa919a8aad
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:02 +0300

    Create Class1.cs

---
+++ задача 13/Class1.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        for (int i = 1; i <= 300; i++)
+        {
+            int deliteli = 0;
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    deliteli += 1;
+                }
+            }
+            if (deliteli == 5)
+            {
+                Console.WriteLine($"Кол-во делителей числа {i} равно " + deliteli.ToString());
+            }
+        }
+    }
+}
