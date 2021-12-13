4e5b5b4156256a412b515c87f892e51591edec5f
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:17 +0300

    Create Class1.cs

---
+++ задача 17/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine(Perimeter(int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine()), int.Parse(Console.ReadLine())));
+    }
+    static int Perimeter(int a, int b, int c)
+    {
+        int per = a + b + c;
+        return per;
+    }
+}
