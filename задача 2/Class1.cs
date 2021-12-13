a3ed9f10da328bae19347d5e93cdfcdde3434aaf
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:55 +0300

    Create Class1.cs

---
+++ задача 2/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.Write("Введите сумму покупки: ");
+
+        float buyPrice = float.Parse(Console.ReadLine());
+
+        float sale = 0;
+
+        if (buyPrice >= 1000)
+        {
+            sale = 0.05f;
+        }
+        else if (buyPrice >= 500)
+        {
+            sale = 0.03f;
+        }
+
+        buyPrice -= buyPrice * sale;
+
+        Console.WriteLine($"Сумма покупки составила {buyPrice} денежных едениц со скидкой {sale}%");
+    }
+}
