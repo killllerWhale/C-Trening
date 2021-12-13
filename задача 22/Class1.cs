6308069f20f6ff889fa74cded611c6b0fd15ca4e
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:29:02 +0300

    Create Class1.cs

---
+++ задача 22/Class1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int i = 0;
+            while (n > 1)
+            {
+                n = n / 2;
+                i++;
+            }
+            Console.WriteLine("Степень: " + i.ToString());
+        }
+    }
+}
