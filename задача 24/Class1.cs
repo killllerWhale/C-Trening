7764c56fdc9c4aed2b27371b861a9e63604f5f5a
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:29:08 +0300

    Create Class1.cs

---
+++ задача 24/Class1.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        int x = int.Parse(Console.ReadLine());
+        ChangeDigits(ref x);
+    }
+    static void ChangeDigits(ref int n)
+    {
+        int temp = n / 10;
+        n = n % 10;
+        n = (n * 10) + temp;
+    }
+}
