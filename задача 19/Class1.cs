68f2dd2f5d9a361279714d1bde0a2b9989ba767f
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:52 +0300

    Create Class1.cs

---
+++ задача 19/Class1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+class Program
+
+{
+    //В строке удалить символ точку (.) и подсчитать количество удаленных символов.
+    static void Main(string[] args)
+    {
+        string rus = Convert.ToString(Console.ReadLine());
+        int counter = 0;
+
+        for (int i = 0; i < rus.Length; i++)
+        {
+            if (rus[i] == '.')
+            {
+                counter++;
+            }
+        }
+        Console.WriteLine(rus.Replace(".", ""));
+        Console.WriteLine(counter);
+    }
+}
