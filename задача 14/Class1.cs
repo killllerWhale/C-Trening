1033ef3b10830a99d26c495654bbcfa1d791bfdd
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:08 +0300

    Create Class1.cs

---
+++ задача 14/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+class Program
+
+{
+    static void Main(string[] args)
+    {
+        int d = Convert.ToInt32(Console.ReadLine());
+
+        for (int i = 1; i <= d; i++)
+        {
+            string deliteli = $"{i} ";
+            for (int j = 1; j <= i; j++)
+            {
+                if (i % j == 0)
+                {
+                    deliteli += "+";
+                }
+            }
+            Console.WriteLine(deliteli);
+        }
+    }
+}
