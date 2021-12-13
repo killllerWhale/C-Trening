7f7bb3a8cc9178af62c407a855f6e99795af2973
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:41 +0300

    Create Class1.cs

---
+++ задача 7/Class1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите сумму покупки");
+            int n = Convert.ToInt32(Console.ReadLine());
+            if (n > 500 && n < 1000)
+            {
+                Console.WriteLine("Стоимость вашей покупки с учетом скидки " + (n - (n * 0.03)).ToString());
+            }
+            else if (n > 1000)
+            {
+                Console.WriteLine("Стоимость вашей покупки с учетом скидки " + (n - (n * 0.05)).ToString());
+            }
+        }
+    }
+}
