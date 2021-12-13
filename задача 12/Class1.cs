aef4896e0cfc3d28aaeba194ae95713743dccbf9
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:27:56 +0300

    Create Class1.cs

---
+++ задача 12/Class1.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите число");
+            int p = Convert.ToInt32(Console.ReadLine());
+            int i = p;
+            int fact = 1;
+            do
+            {
+                fact *= i;
+                --i;
+            }
+            while (i > 0);
+            Console.WriteLine($"{p}! = {fact}");
+            Console.ReadLine();
+        }
+    }
+}
