0d368e3f6348b7c058455e9f8528408dd7338c66
killllerWhale <93337303+killllerWhale@users.noreply.github.com>
2021-12-13 13:28:57 +0300

    Create Class1.cs

---
+++ задача 20/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace CSharp_lessons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите размер массива");
+            int n = Convert.ToInt32(Console.ReadLine());
+            int[] array = new int[n];
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine($"Введите {i + 1} элемент массива");
+                array[i] = int.Parse(Console.ReadLine());
+            }
+            for (int i = 0; i < n; i++)
+            {
+                if (array[i] > 0)
+                {
+                    Console.Write(array[i].ToString() + ' ');
+                }
+            }
+        }
+    }
+}
